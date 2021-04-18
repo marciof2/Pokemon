0422f821a42a42708a1bfb94241e20284ee438b6
marciof2 <marcinsilva26@gmail.com>
2021-04-18 13:01:30 -0300

    StartProject Pokemon

--- lib/poke_view.dart
+++ lib/poke_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pokemo/poke_controller.dart';
-import 'package:pokemo/pokemon.dart';
 
 class PokeView extends StatefulWidget {
   @override
@@ -8,26 +6,8 @@ class PokeView extends StatefulWidget {
 }
 
 class _PokeViewState extends State<PokeView> {
-  final controller = PokeController();
-
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FutureBuilder<Pokemon>(
-            future: controller.pokemon,
-            builder: (context, snapshot) {},
-          ),
-          ElevatedButton(
-            onPressed: () {
-              controller.loadPokemon();
-            },
-            child: const Text('Load Pokemon'),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
